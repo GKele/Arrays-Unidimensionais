d51cdb79f998ff804acc1be1b955d303e1285a0a
Geize Kele <ge.kele@hotmail.com>
2022-11-13 11:09:12 -0300

    Excicio01 finished

--- Arrays/src/exercicios/Ex01.java
+++ Arrays/src/exercicios/Ex01.java
@@ -3,8 +3,17 @@ package exercicios;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Criar array
+		int[] array1 = new int[11];
+		
+		//Looping para gerar e adicionar os números no array (de 10 a 1)
+		for (int i = 10; i > 0; i--) {
+			//Armazenagem dos números
+			array1[i] = i;
+			
+			//A cada looping um número é impresso no console
+			System.out.print(array1[i] + " ");
+		}
 	}
 
 }
